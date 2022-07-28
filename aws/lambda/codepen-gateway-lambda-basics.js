70f67310b92392650bb9032ddcb24011f81de6b1
Ahmed Amine Soltani <soltani.ahmedamine2@gmail.com>
2022-07-29 00:09:47 +0100

    aws/test_api_codepen add GET / POST / DELETE Requests

--- aws/lambda/codepen-gateway-lambda-basics.js
+++ aws/lambda/codepen-gateway-lambda-basics.js
@@ -1,6 +1,28 @@
+//  POST Request
 var xhr = new XMLHttpRequest();
-xhr.open('POST', '/home/user-1/Downloads/codepen-gateway-lambda-basics.js');
-xhr.onreadystatechange = function(event) {
-  console.log(event.target);
-}
+xhr.open("POST", "https://ksifiuwwe9.execute-api.eu-west-3.amazonaws.com/dev/first-api");
+xhr.onreadystatechange = function (event) {
+  console.log(event.target.response);
+};
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.send(JSON.stringify({ age: 26, height: 71, income: 2100 }));
+
+
+// GET Request /all /single
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://ksifiuwwe9.execute-api.eu-west-3.amazonaws.com/dev/first-api/all");
+xhr.onreadystatechange = function (event) {
+  console.log(event.target.response);
+};
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.send();
+
+
+// DELETE Request
+var xhr = new XMLHttpRequest();
+xhr.open("DELETE", "https://ksifiuwwe9.execute-api.eu-west-3.amazonaws.com/dev/first-api");
+xhr.onreadystatechange = function (event) {
+  console.log(event.target.response);
+};
+xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send();
